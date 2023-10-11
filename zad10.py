5f0cc4c5661758557278e41524b32c2a771eec99
JanGasienica <147086351+JanGasienica@users.noreply.github.com>
2023-10-11 12:53:26 +0200

    Add files via upload

---
+++ zad10.py
@@ -0,0 +1,5 @@
+name = "Janek"
+age = 18
+height = 186
+print("MY name is ", name, "I am ", age, "y old. My height is ", height, "In 6 years I will be ", age +6)
+print(f"MY name is {name} I am {age} years old. My height is {height} In 6 years I will be  {age +6}")
---
+++ zad13.py
@@ -0,0 +1,3 @@
+cube_side = int(input("Lenght of cube side: "))
+Surface_area = 6*cube_side**2
+print(f"The surface area of a cube with side {cube_side} is {Surface_area}")
