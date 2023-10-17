f886b52c454253d819c2f1cfc4e469d1819016e7
JanGasienica <147086351+JanGasienica@users.noreply.github.com>
2023-10-17 21:59:33 +0200

    Delete zad11.py

--- zad11.py
+++
@@ -1,6 +0,0 @@
-father_income = int(input("father income: "))
-mother_income = int(input("mother income:  "))
-number_of_family_members= int(input("number of  family members: "))
-totalincome = mother_income + father_income
-income_per_person = totalincome/number_of_family_members
-print(income_per_person)
